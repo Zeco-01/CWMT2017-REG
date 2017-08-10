9f60a100c0dd0646af0bb8a3c95e9d746781ce53
{ZehuaLee} <leezehua@outlook.com>
2017-08-11 00:42:33 +0900

    trial

--- CWMT/user_login/views.py
+++ CWMT/user_login/views.py
@@ -3,15 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 
 def user_login_view(request):
 
     return render(request,'reg.html')
 
+
+@csrf_exempt
 def record(request):
     a = request.POST
-    return HttpResponse(a)
+    return HttpResponse("1234567")
 
 
 # Create your views here.
