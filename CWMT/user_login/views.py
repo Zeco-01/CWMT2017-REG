18151a86d123b491f682ce9b69980e4edd4d9048
{ZehuaLee} <leezehua@outlook.com>
2017-08-12 00:14:38 +0900

    db debuged

--- CWMT/user_login/views.py
+++ CWMT/user_login/views.py
@@ -26,8 +26,8 @@ class Processor(object):
             a = json_data
         if request.method == 'GET':
             raise SyntaxError('GET request cannot use')
-        default = {'name' : a['name'], 'phone' : a['phone'], 'sex':a['sex'], 'phone': str(a['phone']), 'mail': a['email'], 'invoice': a['invoice'],'tax_id': str(a['tax_id']), 'address': a['address'], 'user_id': str(a['id']), 'paper_id': str(a['paper_id']),'stay': a['stay'], 'type' : a['type']}
-        user, if_success = User.objects.get_or_create(mail = str(a['email']), defaults = default)
+        default = {'name' : a['name'], 'phone' : a['phone'], 'sex':a['sex'], 'phone': a['phone'], 'mail': a['email'], 'invoice': a['invoice'],'tax_id': a['tax_id'], 'address': a['address'], 'user_id': a['id'], 'paper_id': a['paper_id'],'stay': a['stay'], 'type' : a['type']}
+        user, if_success = User.objects.get_or_create(mail = a['email'], defaults = default)
 
         if if_success == True:
             if a['stay'] == 'no':
@@ -43,7 +43,23 @@ class Processor(object):
                 user.out_date = str(a['out_date'])
                 user.m_room = str(a['m_name'])
             user.save()
-            strr = 'name: ' + user.name + '<br />' + 'register number: '+ str(user.register_number)+'\n'
+            strr =  '姓名: ' + user.name + '    '+ user.sex +'<br />' + '注册码: '+ str(user.register_number)+'<br />'
+            strr += '电话: '+user.phone + '<br />'
+            strr += 'e-mail: '+ user.mail + '<br />'
+            strr += '发票抬头: ' + user.invoice + '<br />'
+            strr += '发票纳税人识别号: ' + user.tax_id + '<br />'
+            strr += '发票邮寄地址: ' + user.address + '<br />'
+            strr += '身份证号: '+ user.user_id + '<br />'
+            strr += '论文ID: '+ user.paper_id + '<br />'
+            strr += '住宿方式: '
+            if user.stay == 'no':
+                strr += '自行解决'
+            if user.stay == 'single':
+                strr += '单间（350元 /人＊天）' + '<br />'
+                strr += '入住日期: '+ user.in_date + ' - '+ user.out_date + '<br />'
+            if user.stay == 'multi':
+                strr += '合住（两人每天390元）'
+                strr += '入住日期: '+ user.in_date + ' - ' + user.out_date + '<br />'
         else:
             strr = "create user failed because identical object exists: register number is: "+str(user.register_number)
         return HttpResponse(strr)
