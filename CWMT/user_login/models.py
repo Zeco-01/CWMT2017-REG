f3d9e2c476af7d9dada16418fc83ae9242b5a290
{ZehuaLee} <leezehua@outlook.com>
2017-08-11 18:05:41 +0900

    register_code

--- CWMT/user_login/models.py
+++ CWMT/user_login/models.py
@@ -21,6 +21,7 @@ class User(models.Model):
     type = models.CharField(max_length= 200, blank = True) # user type
     enter_date = models.CharField(max_length=100, blank = True) # login date
     out_date = models.CharField(max_length=100, blank = True) # logout date
+    register_number = models.CharField(max_length=20, blank = True, primary_key=True)
 
     def __str__(self):
         return self.name
