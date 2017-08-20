f8a58b02ae6d40fb3553049d916b2fe23bc160e4
Zeco <zhangcong002@gmail.com>
2017-08-20 20:42:44 +0800

    add some fileds

--- CWMT/user_login/models.py
+++ CWMT/user_login/models.py
@@ -22,6 +22,10 @@ class User(models.Model):
     in_date = models.CharField(max_length=100,null = True) # login date
     out_date = models.CharField(max_length=100,null=True) # logout date
     register_number = models.AutoField(unique=True, max_length=20,blank = True,primary_key=True)
+    invoice_type = models.CharField(max_length = 10,blank=True)
+    invoice_bank = models.CharField(max_length=100,null=True,blank=True)
+    invoice_id = models.CharField(max_length=100,null=True,blank=True)
+    invoice_tel = models.CharField(max_length=60,null=True,blank=True)
 
     def __str__(self):
         return self.name
